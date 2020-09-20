ea419ad98691d004d74467c359ae5653408d161c
lutsker <vllutsker@googlemail.com>
2020-09-20 10:37:26 +0200

    refactor fromroman

--- src/fromroman/fromroman.cpp
+++ src/fromroman/fromroman.cpp
@@ -2,26 +2,45 @@
 #include <map>
 #include <vector>
 
-std::map<char, int> literal_map = {{'I', 1}, {'V', 5}, {'X', 10},
-					  {'L', 50}, {'C', 100}, {'D', 500},
-					  {'M', 1000}};
 
-int from_roman(std::string roman_number) {
-
-  std::vector<int> symbols;
+int sum(const std::vector<int>& summands)
+{
   int result = 0;
+  for (auto summand: summands)
+    result += summand;
+  return result;
+}
+
+
+std::vector<int> map_roman_digits_to_their_values(std::string roman_number)
+{
+  std::map<char, int> literal_map = {
+    {'I', 1},  {'V', 5}, {'X', 10},
+	  {'L', 50}, {'C', 100}, {'D', 500},
+	  {'M', 1000}
+  };
+  std::vector<int> symbols;
   
-  for (auto symbol: roman_number) {
+  for (auto symbol: roman_number)
     symbols.push_back(literal_map.at(symbol));
-  }
-  if (symbols.size() > 1) {
-    for (int ii = 0; ii < symbols.size() - 1; ii++) {
-      if (symbols[ii+1] - symbols[ii] > 0)
-	symbols[ii] *= -1;
+  return symbols;
+}
+
+
+void make_next_lower_rank_digit_negative(std::vector<int>& digits)
+{
+  if (digits.size() > 1) {
+    for (int ii = 0; ii < digits.size() - 1; ii++) {
+      if (digits[ii+1] - digits[ii] > 0)
+      	digits[ii] *= -1;
     }
   }
-  for (auto symbol: symbols) {
-    result += symbol;
-  }
-  return result;
+}
+
+
+int from_roman(std::string roman_number) 
+{
+  std::vector<int> digits = map_roman_digits_to_their_values(roman_number);
+  make_next_lower_rank_digit_negative(digits);  
+  return sum(digits);
 }
